29f561ca303540e7452776b4e8896683124283d7
leticiabarrosProjeto <leticiabaarros97@gmail.com>
2023-05-23 07:58:33 -0300

    Criação sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 //JSX =JavaScript + XML (HTML)
 import { Header } from './components/Header';
 import { Post } from './Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
 
 export function App() {
   return (
     <div> 
-
       <Header />
       
-      <Post 
+      <div className={styles.wrapper}>
+
+      <Sidebar />
+        <main>
+        <Post 
       author="Leticia Barros"
       content="Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem "/>
       <Post 
@@ -19,6 +27,8 @@ export function App() {
       content="Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem "/>
       <Post />
       <Post />
+        </main>
+      </div>  
     </div>
   )
 }
