662d99054f302bc9a147ee7c767f498f02d0e4c6
leticiabarrosProjeto <leticiabaarros97@gmail.com>
2023-05-24 13:44:52 -0300

    Iteração, propriedades do Post

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,42 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+         id: 1,
+         author:{
+         avatarUrl:'https://github.com/LeticiabarrosProjeto.png',
+         name: 'Leticia Barros',
+         role:'Web Developer', 
+  },
+         content: [
+    { type: 'paragraph', content: 'Fala Galera 👋🏻',}, 
+    { type: 'paragraph', content: 'Acabei de subor mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da'},
+    { type: 'paragraph', content:  'Rocketseat. O nome do projeto é DoctorCare.'},
+    { type: 'link', content:'ane.design/doctorcare'},
+  ],
+         publishedAt: new Date('2023-05-24 16:00:00'),
+ },
+  {
+    id: 2,
+      author:{
+      avatarUrl:'https://images.unsplash.com/photo-1661956600684-97d3a4320e45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+      name: 'Hector Barros',
+      role:'Back-end developer', 
+  },
+  content: [
+    { type: 'paragraph', content: 'Fala Galera 👋🏻',}, 
+    { type: 'paragraph', content: 'Acabei de subor mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da'},
+    { type: 'paragraph', content:  'Rocketseat. O nome do projeto é DoctorCare.'},
+    { type: 'link', content:'ane.design/doctorcare'},
+  ],
+  publishedAt: new Date('2023-05-23 18:00:00'),
+ },
+];
+ 
+
+
+
 export function App() {
   return (
     <div> 
@@ -16,17 +52,15 @@ export function App() {
 
       <Sidebar />
         <main>
-        <Post 
-      author="Leticia Barros"
-      content="Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem "/>
-      <Post 
-      author="Leticia Barros"
-      content="Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem "/>
-      <Post 
-      author="Leticia Barros"
-      content="Lorem LoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLoremLorem "/>
-      <Post />
-      <Post />
+        {posts.map(post => {
+          return (
+           <Post 
+          author={post.author}
+          content={post.content}
+          publishedAt={post.publishedAt}
+           />
+          )
+        })}
         </main>
       </div>  
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,33 +1,44 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { 
+    locale: ptBR,
+    addSuffix: true,
+  })
+
     return (
         <article className={styles.post}>
             <header>
                <div className={styles.author}>
-                 <Avatar src="https://github.com/LeticiabarrosProjeto.png"/>
+                 <Avatar src={author.avatarUrl}/>
                  <div className={styles.authorInfo}>
-                    <strong>Leticia Barros</strong>
-                    <span>Font-end</span>
+                    <strong>{author.name}</strong>
+                    <span>{author.role}</span>
                  </div>
                </div>
 
-               <time title="23 de Maio às 13:11h" dateTime="2023-05-11 13:11:20">Publicado há 1h </time>
+              <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                {publishedDateRelativeToNow}
+              </time>
             </header>
 
             <div className={styles.content}>
-
-            <p>Fala Galera 👋🏻 </p>
-            <p>Acabei de subor mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da</p>
-            <p>Rocketseat. O nome do projeto é DoctorCare.</p>
-            <p><a href=""> jane.design/doctorcare</a></p>
-            <p>
-                <a href="">#novoprojeto</a>{' '}
-                <a href=''>#nlw</a>{' '}
-                <a href=''>#rocketseat</a>
-            </p>
+              {content.map(line => {
+                if (line.type == 'paragraph') {
+                  return <p>{line.content}</p>;
+                } else if (line.type == 'link') {
+                  return <p><a href="#">{line.content}</a></p>
+                }
+              })}
+            
             </div>
          <form className={styles.commentForm}> 
            <strong>Deixe seu feedback</strong>
